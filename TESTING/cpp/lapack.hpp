d3078be1512e7a08ec9f84329ef91ce2ffe38803
Christoph Conrads <gitlab@christoph-conrads.name>
2020-05-07 21:53:25 +0200

    ZGGQRCS: update C declaration after API changes

--- TESTING/cpp/lapack.hpp
+++ TESTING/cpp/lapack.hpp
@@ -139,21 +139,21 @@ extern "C"
 	);
 
 	void zggqrcs_(
-		char* jobu1, char* jobu2, char* jobqt,
+		char* jobu1, char* jobu2, char* jobx,
 		lapack_int* m, lapack_int* n, lapack_int* p,
-		double* w, lapack_int* l,
+		lapack_int* l, lapack_int* swapped_p,
 		std::complex<double>* A, lapack_int* lda,
 		std::complex<double>* B, lapack_int* ldb,
-		double* theta,
+		double* alpha, double* beta,
 		std::complex<double>* U1, lapack_int* ldu1,
 		std::complex<double>* U2, lapack_int* ldu2,
-		std::complex<double>* Qt, lapack_int* ldqt,
 		std::complex<double>* work, lapack_int* lwork,
 		double* rwork, lapack_int* lrwork,
 		lapack_int* iwork,
 		lapack_int* info,
 		std::size_t jobu1_len, std::size_t jobu2_len, std::size_t jobqt_len
 	);
+
 
 
 	void slasrti_(
@@ -536,6 +536,40 @@ inline integer_t ggqrcs(
 	integer_t info = -1;
 	integer_t swapped_p = -1;
 	cggqrcs_(
+		&jobu1, &jobu2, &jobx,
+		&m, &n, &p, p_l, &swapped_p,
+		A, &lda, B, &ldb,
+		p_alpha, p_beta,
+		U1, &ldu1, U2, &ldu2,
+		work, &lwork, rwork, &lrwork, iwork, &info,
+		1, 1, 1);
+	*p_swapped_p = swapped_p != 0;
+	return info;
+}
+
+inline integer_t ggqrcs(
+	char jobu1, char jobu2, char jobx,
+	integer_t m, integer_t n, integer_t p, integer_t* p_l,
+	bool* p_swapped_p,
+	std::complex<double>* A, integer_t lda,
+	std::complex<double>* B, integer_t ldb,
+	double* p_alpha, double* p_beta,
+	std::complex<double>* U1, integer_t ldu1,
+	std::complex<double>* U2, integer_t ldu2,
+	std::complex<double>* work, integer_t lwork,
+	double* rwork, integer_t lrwork,
+	integer_t* iwork)
+{
+	assert( p_l );
+	assert( p_swapped_p );
+	assert( p_alpha );
+	assert( p_beta );
+	assert( work );
+	assert( rwork );
+
+	integer_t info = -1;
+	integer_t swapped_p = -1;
+	zggqrcs_(
 		&jobu1, &jobu2, &jobx,
 		&m, &n, &p, p_l, &swapped_p,
 		A, &lda, B, &ldb,
