22e41925c9f9cee94fff5114e6b349e0188204b2
weslley.spereira <weslley.spereira@gmail.com>
2021-08-27 17:24:45 -0600

    Adds tests with Infs and NaNs

--- TESTING/AUX/zabs.f
+++ TESTING/AUX/zabs.f
@@ -1,29 +1,53 @@
-*  (1) x = b/2, where b is the Blue's min constant. (b/2)^2 underflows but the norm is b/2.
-*  (2) x = UN, where UN is the underflow threshold. UN^2 underflows but the norm is UN.
-*  (3) x = SM, where SM is the smallest subnormal number. SM^2 underflows but the norm is SM.
-*      Mind that not all platforms might implement subnormal numbers.
-*  (4) x = 2*B, where B is the Blue's max constant. (2*B)^2 overflows but the norm is 2*B.
-*  (5) x = OV, where OV is the overflow threshold. OV^2 overflows but the norm is OV.
+*> \brief zabs tests the robustness and precision of the intrinsic ABS for double complex 
+*> \author Weslley S. Pereira, University of Colorado Denver, U.S.
 *
-*  (a) y = x + 0 * I, |y| = x
-*  (b) y = 0 + x * I, |y| = x
-*  (c) y = (3/4)*x + x * I, |y| = (5/4)*x whenever (3/4)*x and (5/4)*x can be exactly stored
-*  (d) y = (1/2)*x + (1/2)*x * I, |y| = (1/2)*x*sqrt(2) whenever (1/2)*x can be exactly stored
+*> \verbatim
+*>
+*> Real values for test:
+*> (1) x = b/2, where b is the Blue's min constant. (b/2)^2 underflows but the norm is b/2.
+*> (2) x = UN, where UN is the underflow threshold. UN^2 underflows but the norm is UN.
+*> (3) x = SM, where SM is the smallest subnormal number. SM^2 underflows but the norm is SM.
+*>     Mind that not all platforms might implement subnormal numbers.
+*> (4) x = 2*B, where B is the Blue's max constant. (2*B)^2 overflows but the norm is 2*B.
+*> (5) x = OV, where OV is the overflow threshold. OV^2 overflows but the norm is OV.
+*>
+*> Tests:
+*> (a) y = x + 0 * I, |y| = x
+*> (b) y = 0 + x * I, |y| = x
+*> (c) y = (3/4)*x + x * I, |y| = (5/4)*x whenever (3/4)*x and (5/4)*x can be exactly stored
+*> (d) y = (1/2)*x + (1/2)*x * I, |y| = (1/2)*x*sqrt(2) whenever (1/2)*x can be exactly stored
+*>
+*> Special cases:
+*>
+*> (e) Inf propagation
+*>    y = Inf + 0 * I, |y| is Inf.
+*>    y =-Inf + 0 * I, |y| is Inf.
+*>    y = 0 + Inf * I, |y| is Inf.
+*>    y = 0 - Inf * I, |y| is Inf.
+*>    y = Inf + Inf * I, |y| is Inf.
+*>
+*> (f) NaN propagation
+*>    y = NaN + 0 * I, |y| is NaN.
+*>    y = 0 + NaN * I, |y| is NaN.
+*>    y = NaN + NaN * I, |y| is NaN.
+*>
+*> \endverbatim
 *
       program zabs
 
-      integer           N, i
-      parameter       ( N = 5 )
+      integer           N, i, nNaN, nInf
+      parameter       ( N = 5, nNaN = 3, nInf = 5 )
 
       double precision  X( N ), R, threeFourth, fiveFourth, answerC(N),
-     $                  answerD(N), oneHalf
+     $                  answerD(N), oneHalf, aInf, aNaN
       parameter       ( threeFourth = 3.0d0 / 4,
      $                  fiveFourth = 5.0d0 / 4,
      $                  oneHalf = 1.0d0 / 2 )
 
-      double complex    Y
+      double complex    Y, cInf( nInf ), cNaN( nNaN )
       intrinsic         ABS, DBLE, RADIX, CEILING, TINY, DIGITS, SQRT,
-     $                  MAXEXPONENT, MINEXPONENT, FLOOR, HUGE, DCMPLX
+     $                  MAXEXPONENT, MINEXPONENT, FLOOR, HUGE, DCMPLX,
+     $                  ACOS
 *
       X(1) = DBLE(RADIX(0.0d0))**CEILING(
      $            (MINEXPONENT(0.0d0) - 1) * 0.5d0 )
@@ -32,6 +56,18 @@
       X(4) = DBLE(RADIX(0.0d0))**FLOOR(
      $            (MAXEXPONENT(0.0d0) - DIGITS(0.0d0) + 1) * 0.5d0 )
       X(5) = HUGE(0.0d0)
+*
+      aInf = X(5) * 2
+      cInf(1) = DCMPLX( aInf, 0.0d0 )
+      cInf(2) = DCMPLX(-aInf, 0.0d0 )
+      cInf(3) = DCMPLX( 0.0d0, aInf )
+      cInf(4) = DCMPLX( 0.0d0,-aInf )
+      cInf(5) = DCMPLX( aInf,  aInf )
+*
+      aNaN = aInf / aInf
+      cNaN(1) = DCMPLX( aNaN, 0.0d0 )
+      cNaN(2) = DCMPLX( 0.0d0, aNaN )
+      cNaN(3) = DCMPLX( aNaN,  aNaN )
 *
       answerC(1) = fiveFourth * X(1)
       answerC(2) = fiveFourth * X(2)
@@ -82,6 +118,27 @@
               WRITE( *, FMT = 9999 ) Y, R, answerD(i)
           endif
   40  continue
+*
+*     Test (e) Infs
+      do 50 i = 1, nInf
+          Y = cInf(i)
+          R = ABS( Y )
+          if( R .le. HUGE(0.0d0) ) then
+              WRITE( *, FMT = 9999 ) Y, R, aInf
+          endif
+  50  continue
+*
+*     Test (f) NaNs
+      do 60 i = 1, nNaN
+          Y = cNaN(i)
+          R = ABS( Y )
+          if( R .eq. R ) then
+              WRITE( *, FMT = 9998 ) Y, R
+          endif
+  60  continue
+*
+ 9998 FORMAT( ' ** ABS( ', (ES10.3,SP,ES10.3,"*I"), ' ) = ', ES10.3,
+     $        ' is not a NaN' )
 *
  9999 FORMAT( ' ** ABS( ', (ES10.3,SP,ES10.3,"*I"), ' ) = ', ES10.3,
      $        ' differs from ', ES10.3 )
--- TESTING/AUX/zdiv.f
+++ TESTING/AUX/zdiv.f
@@ -1,27 +1,62 @@
-*  (1) x = b/2, where b is the Blue's min constant. (b/2)^2 underflows but the norm is b/2.
-*  (2) x = UN, where UN is the underflow threshold. UN^2 underflows but the norm is UN.
-*  (3) x = SM, where SM is the smallest subnormal number. SM^2 underflows but the norm is SM.
-*      Mind that not all platforms might implement subnormal numbers.
-*  (4) x = 2*B, where B is the Blue's max constant. (2*B)^2 overflows but the norm is 2*B.
-*  (5) x = OV, where OV is the overflow threshold. OV^2 overflows but the norm is OV.
+*> \brief zdiv tests the robustness and precision of the double complex division
+*> \author Weslley S. Pereira, University of Colorado Denver, U.S.
 *
-*  (a) y = x + 0 * I, y/y = 1
-*  (b) y = 0 + x * I, y/y = 1
-*  (c) y = x + x * I, y/y = 1
-*  (d) y1 = 0 + x * I, y2 = 0 + x * I, y1/y2 = I
-*  (e) y1 = 0 + x * I, y2 = 0 + x * I, y2/y1 = -I
-*  (f) y = x + x * I, y/conj(y) = I
+*> \verbatim
+*>
+*> Real values for test:
+*> (1) x = b/2, where b is the Blue's min constant. (b/2)^2 underflows but the norm is b/2.
+*> (2) x = UN, where UN is the underflow threshold. UN^2 underflows but the norm is UN.
+*> (3) x = SM, where SM is the smallest subnormal number. SM^2 underflows but the norm is SM.
+*>     Mind that not all platforms might implement subnormal numbers.
+*> (4) x = 2*B, where B is the Blue's max constant. (2*B)^2 overflows but the norm is 2*B.
+*> (5) x = OV, where OV is the overflow threshold. OV^2 overflows but the norm is OV.
+*>
+*> Tests:
+*> (a) y = x + 0 * I, y/y = 1
+*> (b) y = 0 + x * I, y/y = 1
+*> (c) y = x + x * I, y/y = 1
+*> (d) y1 = 0 + x * I, y2 = x + 0 * I, y1/y2 = I
+*> (e) y1 = 0 + x * I, y2 = x + 0 * I, y2/y1 = -I
+*> (f) y = x + x * I, y/conj(y) = I
+*>
+*> Special cases:
+*>
+*> (g) Inf inputs:
+*>    y = ( Inf + 0   * I)
+*>    y = ( 0   + Inf * I)
+*>    y = (-Inf + 0   * I)
+*>    y = ( 0   - Inf * I)
+*>    y = ( Inf + Inf * I)
+*> Tests:
+*>    0 / y is either 0 or NaN.
+*>    1 / y is either 0 or NaN.
+*>    y / y is NaN.
+*>
+*> (h) NaN inputs:
+*>    y = (NaN + 0   * I)
+*>    y = (0   + NaN * I)
+*>    y = (NaN + NaN * I)
+*> Tests:
+*>    0 / y is NaN.
+*>    1 / y is NaN.
+*>    y / y is NaN.
+*>
+*> \endverbatim
 *
       program zdiv
 
-      integer           N, i
-      parameter       ( N = 5 )
+      integer           N, i, nNaN, nInf
+      parameter       ( N = 5, nNaN = 3, nInf = 5 )
 
-      double precision  X( N ), threeFourth, fiveFourth
+      double precision  X( N ), threeFourth, fiveFourth, aInf, aNaN
       parameter       ( threeFourth = 3.0d0 / 4,
      $                  fiveFourth = 5.0d0 / 4 )
 
-      double complex    Y, Y2, R
+      double complex    Y, Y2, R, cInf( nInf ), cNaN( nNaN ), czero,
+     $                  cone
+      parameter       ( czero = DCMPLX( 0.0d0, 0.0d0 ),
+     $                  cone  = DCMPLX( 1.0d0, 0.0d0 ) )
+*
       intrinsic         DCONJG, DBLE, RADIX, CEILING, TINY, DIGITS,
      $                  MAXEXPONENT, MINEXPONENT, FLOOR, HUGE, DCMPLX
 *
@@ -32,6 +67,18 @@
       X(4) = DBLE(RADIX(0.0d0))**FLOOR(
      $            (MAXEXPONENT(0.0d0) - DIGITS(0.0d0) + 1) * 0.5d0 )
       X(5) = HUGE(0.0d0)
+*
+      aInf = X(5) * 2
+      cInf(1) = DCMPLX( aInf, 0.0d0 )
+      cInf(2) = DCMPLX(-aInf, 0.0d0 )
+      cInf(3) = DCMPLX( 0.0d0, aInf )
+      cInf(4) = DCMPLX( 0.0d0,-aInf )
+      cInf(5) = DCMPLX( aInf,  aInf )
+*
+      aNaN = aInf / aInf
+      cNaN(1) = DCMPLX( aNaN, 0.0d0 )
+      cNaN(2) = DCMPLX( 0.0d0, aNaN )
+      cNaN(3) = DCMPLX( aNaN,  aNaN )
 *
 *     Test (a) y = x + 0 * I, y/y = 1
       do 10 i = 1, N
@@ -48,7 +95,6 @@
           R = Y / Y
           if( R .ne. 1.0D0 ) then
               WRITE( *, FMT = 9999 ) Y, Y, R, 1.0D0
-              print *, Y, R
           endif
   20  continue
 *
@@ -58,7 +104,6 @@
           R = Y / Y
           if( R .ne. 1.0D0 ) then
               WRITE( *, FMT = 9999 ) Y, Y, R, 1.0D0
-              print *, Y, R
           endif
   30  continue
 *
@@ -69,7 +114,6 @@
           R = Y / Y2
           if( R .ne. DCMPLX(0.0D0,1.0D0) ) then
               WRITE( *, FMT = 9999 ) Y, Y2, R, DCMPLX(0.0D0,1.0D0)
-              print *, Y, Y2, R
           endif
   40  continue
 *
@@ -80,7 +124,6 @@
           R = Y2 / Y
           if( R .ne. DCMPLX(0.0D0,-1.0D0) ) then
               WRITE( *, FMT = 9999 ) Y2, Y, R, DCMPLX(0.0D0,-1.0D0)
-              print *, Y, Y2, R
           endif
   50  continue
 *
@@ -91,9 +134,46 @@
           if( R .ne. DCMPLX(0.0D0,1.0D0) ) then
               WRITE( *, FMT = 9999 ) Y, DCONJG( Y ), R,
      $                               DCMPLX(0.0D0,1.0D0)
-              print *, Y, R
           endif
   60  continue
+*
+*     Test (g) Infs
+      do 70 i = 1, nInf
+          Y = cInf(i)
+          R = czero / Y
+          if( (R .ne. czero) .and. (R .eq. R) ) then
+              WRITE( *, FMT = 9998 ) czero, Y, R
+          endif
+          R = cone / Y
+          if( (R .ne. 0.0d0) .and. (R .eq. R) ) then
+              WRITE( *, FMT = 9998 ) cone, Y, R
+          endif
+          R = Y / Y
+          if( R .eq. R ) then
+              WRITE( *, FMT = 9998 ) Y, Y, R
+          endif
+  70  continue
+*
+*     Test (h) NaNs
+      do 80 i = 1, nNaN
+          Y = cNaN(i)
+          R = czero / Y
+          if( R .eq. R ) then
+              WRITE( *, FMT = 9998 ) czero, Y, R
+          endif
+          R = cone / Y
+          if( R .eq. R ) then
+              WRITE( *, FMT = 9998 ) cone, Y, R
+          endif
+          R = Y / Y
+          if( R .eq. R ) then
+              WRITE( *, FMT = 9998 ) Y, Y, R
+          endif
+  80  continue
+*
+ 9998 FORMAT( ' ** (', (ES10.3,SP,ES10.3,"*I"), ' ) / ( ',
+     $                 (ES10.3,SP,ES10.3,"*I"), ' ) = ',
+     $                 (ES10.3,SP,ES10.3,"*I"), ' is unexpected' )
 *
  9999 FORMAT( ' ** (', (ES10.3,SP,ES10.3,"*I"), ' ) / ( ',
      $                 (ES10.3,SP,ES10.3,"*I"), ' ) = ',
